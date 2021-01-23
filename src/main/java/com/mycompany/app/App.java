8557b7866b9903f6992472f5c112d03d000099af
anirudhnadgod <anirudhnadgod@users.noreply.github.com>
2021-01-23 09:51:10 +0530

    095023012021-my-app git project
    095023012021-my-app git project

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!, v 2.0 changed by AN" );
+        System.out.println( "Hello World!, v 3.0 changed by AN" );
     }
 }
